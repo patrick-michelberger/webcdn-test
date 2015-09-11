5a60a0dd967afa3df194ec35787d76aa1137a5df
patrick_michelberger <pmichelberger@gmx.de>
2015-09-11 18:28:43 +0200

    ABOUTYOU sdk added

---
+++ aboutyou.js
@@ -0,0 +1,46 @@
+var config = require('./config.js');
+
+AY = require('aboutyou-sdk')(config.AY.appId, config.AY.secret);
+
+var AboutYou = {};
+
+AboutYou.fetchProducts = function(categoryId, limit, productFields) {
+    var args = [].slice.call(arguments);
+    var callback = typeof args[args.length - 1] === 'function' && args.pop();
+
+    // Search options
+    categoryId = categoryId && (typeof categoryId !== 'function') ? categoryId :  93924;
+    limit = limit && (typeof limit !== 'function') ? limit :  100;
+    productFields = productFields && (typeof productFields !== 'function') ? productFields : ["id", "name", "default_image"];
+
+    // Create search criteria
+    var criteria = AY.getProductSearchCriteria('GENERATE_OWN_SESSION');
+    criteria.filterByCategoryIds(categoryId);
+    criteria.selectProductFields(productFields);
+    criteria.setLimit(limit);
+
+    // Execute search
+    AY.fetchProductSearch(criteria, function(err, searchResult) {
+        if (err) {
+            callback(err, null);
+        } else {
+            var products = searchResult ? searchResult.toJSON() : [];
+            callback(null, formatProducts(products));
+        }
+    });
+};
+
+var formatProducts = function(products) {
+    var results = [];
+    for (var i = 0; i < products.length; i++) {
+    	console.log("products[i]: ", products[i]);
+        results.push({
+            "id": products[i].id,
+            "name": products[i].name,
+            "image_url": products[i].defaultImage.url
+        });
+    };
+    return results;
+};
+
+module.exports = AboutYou;
--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+var AY = require('./aboutyou.js');
+
+AY.fetchProducts(function(err, products) {
+    console.log("found products: ", products);
+});
+
+app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
     var host = server.address().address;
-      var port = server.address().port;
-
-        console.log('Example app listening at http://%s:%s', host, port);
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
 });
